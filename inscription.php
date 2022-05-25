abeccd94c981404797e8c727071411237477396e
BriwixX <eti.brchx@gmail.com>
2022-05-25 17:18:22 +0200

    update adresse inscription

--- inscription.php
+++ inscription.php
@@ -12,6 +12,7 @@
       $prenom = !empty($_POST['prenom']) ? $_POST['prenom'] : NULL;
       $email = !empty($_POST['email']) ? $_POST['email'] : NULL;
       $phone = !empty($_POST['phone']) ? $_POST['phone'] : NULL;
+      $adress = !empty($_POST['adress']) ? $_POST['adress'] : NULL;
       $passw1 = !empty($_POST['password1']) ? $_POST['password1'] : NULL;
       $passw2 = !empty($_POST['password2']) ? $_POST['password2'] : NULL;
 
@@ -19,24 +20,25 @@
       $requete_check_existe_pas = "SELECT id_personne FROM compte WHERE (nom = '".$nom."' AND prenom = '".$prenom."') OR email = '".$email."' OR phone = '".$phone."'";
       $result_exist_pas = mysqli_query($link_login, $requete_check_existe_pas);
 
-      $requete_inscription = "INSERT INTO `compte`(`id_personne`, `nom`, `prenom`, `email`, `phone`, `passw`, `role_`) VALUES (0,'".$nom."','".$prenom."','".$email."','".$phone."','".$passw1."', 'festivalier')";
+      $requete_inscription = "INSERT INTO `compte`(`id_personne`, `nom`, `prenom`, `email`, `phone`, `adresse`, `passw`, `role_`) VALUES (0,'".$nom."','".$prenom."','".$email."','".$phone."','".$adress."','".$passw1."', 'festivalier')";
 
-      
       if(!empty($_POST)){
             if(empty($_POST['nom'])){}
             else if(empty($_POST['prenom'])){}
             else if(empty($_POST['email'])){}
             else if(empty($_POST['phone'])){}
+            else if(empty($_POST['adress'])){}
             else if(empty($_POST['password1'])){}
             else if(empty($_POST['password2'])){}
             else if($_POST['password1'] !== $_POST['password2'])
-                echo "Les 2 mots de passes rentrés sont différents.";
+                echo "Les 2 mots de passes rentres sont differents.";
 
-            // Données CORRECTES : redirige vers la page trajet
+            // Donnees CORRECTES : redirige vers la page trajet
             else if(mysqli_num_rows($result_exist_pas) == 0) {
+                echo $requete_inscription;
                 $result = mysqli_query($link_login, $requete_inscription); // On inscrit le nouveau dans la bdd
                 session_start();
-                $result_exist = mysqli_query($link_login, $requete_check_existe_pas); // On récupère l'id_personne du nouveau
+                $result_exist = mysqli_query($link_login, $requete_check_existe_pas); // On rï¿½cupï¿½re l'id_personne du nouveau
                 $session = mysqli_fetch_assoc($result_exist);
                 $_SESSION['login'] = $session["id_personne"];
                 header("Location: page.php");
@@ -58,6 +60,9 @@
 
         <label>Telephone</label>
         <input type="text" name="phone" id="phone" value="<?php if(!empty($_POST['phone'])) { echo htmlspecialchars($_POST['phone'], ENT_QUOTES); } ?>" required>
+
+        <label>Adresse</label>
+        <input type="text" name="adress" id="adress" value="<?php if(!empty($_POST['adress'])) { echo htmlspecialchars($_POST['adress'], ENT_QUOTES); } ?>" required>
 
         <label>Mot de passe</label>
         <input type="text" name="password1" id="password1" value="<?php if(!empty($_POST['password1'])) { echo htmlspecialchars($_POST['password1'], ENT_QUOTES); } ?>" required>
